9eb2fadb82bfc35c0dea11b2df4ec706c716c0d1
vvvyyynet <levyn.buerki@unibe.ch>
2026-02-21 23:41:23 +0100

    fix: add bibls to TRegister

--- src/lib/types/register/TRegister.ts
+++ src/lib/types/register/TRegister.ts
@@ -1,15 +1,21 @@
-import { type TPlacesKeys } from "./TPlacesKeys";
-import { type TPeopleKeys } from "./TPeopleKeys";
-import { type TEventsKeys } from "./TEventsKeys";
-import { type TKeywordsKeys } from "./TKeywordsKeys";
-import { type TOrganisationsKeys } from "./TOrganisationsKeys";
-import { type TWorksKeys } from "./TWorksKeys";
+import { type TPlacesKeys } from './TPlacesKeys';
+import { type TPeopleKeys } from './TPeopleKeys';
+import { type TEventsKeys } from './TEventsKeys';
+import { type TKeywordsKeys } from './TKeywordsKeys';
+import { type TOrgsKeys } from './TOrgsKeys';
+import { type TSmallformsKeys } from './TSmallformsKeys';
+import { type TLongformsKeys } from './TLongformsKeys';
+import { type TLettersKeys } from './TLettersKeys';
+import type { TBiblsKeys } from './TBiblsKeys';
 
 export type TPeopleTypes = string; //! restrict to subet later
 export type TPlacesTypes = string; //! restrict to subet later
 export type TEventsTypes = string; //! restrict to subet later
-export type TOrganisationsTypes = string; //! restrict to subet later
-export type TWorksTypes = "book" | "article";
+export type TorgsTypes = string; //! restrict to subet later
+export type TSmallformsTypes = 'article' | 'feuilleton';
+export type TLongformsTypes = string; //! restrict to subet later
+export type TLettersTypes = string; //! restrict to subet later
+export type TBiblsTypes = string; //! restrict to subet later
 export type TKeywordsTypes = string; //! restrict to subet later
 
 export type TRegister = {
@@ -69,6 +75,33 @@ export type TRegister = {
 				authorId: TPeopleKeys; //! string to account for uncertainties
 				pubDate: string;
 				type: TWorksTypes;
+				gndNumber?: string; // optional
+			};
+		};
+		longforms: {
+			[key in TSmallformsKeys]: {
+				name: string;
+				authorId: TPeopleKeys; //! string to account for uncertainties
+				pubDate: string;
+				type: TSmallformsTypes;
+				gndNumber?: string; // optional
+			};
+		};
+		letters: {
+			[key in TLettersKeys]: {
+				name: string;
+				authorId: TPeopleKeys; //! string to account for uncertainties
+				pubDate: string;
+				type: TLettersTypes;
+				gndNumber?: string; // optional
+			};
+		};
+		bibls: {
+			[key in TBiblsKeys]: {
+				name: string;
+				type: TLettersTypes;
+				authorId: TPeopleKeys; //! string to account for uncertainties
+				pubDate: string;
 				gndNumber?: string; // optional
 			};
 		};
