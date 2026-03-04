09a5f634e5047d0a4a2afa5f8f09c6c2cd71d916
vvvyyynet <levyn.buerki@unibe.ch>
2026-03-04 15:58:37 +0100

    Revert "TEMP: check whether it works"
    This reverts commit ce9629c0ad5621d01b7587df55eb7a71b33d4f6e.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
-<p class="bg-primary-500">
-	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
-</p>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
