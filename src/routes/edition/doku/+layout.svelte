0310df19fff3721adb11dde3660362dd4cd7bb9b
vvvyyynet <levyn.buerki@unibe.ch>
2026-03-04 16:15:24 +0100

    feat: add layout for all blogs containing global styles

---
+++ src/routes/edition/doku/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	let { children } = $props();
+</script>
+
+{@render children()}
+
+<style>
+	/* Global Styles for the blog */
+	:global(h1) {
+		font-size: xx-large;
+	}
+	:global(h2) {
+		font-size: x-large;
+	}
+	:global(h3) {
+		font-size: large;
+	}
+</style>
