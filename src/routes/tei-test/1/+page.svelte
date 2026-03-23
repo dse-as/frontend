fdb56d3a15e2482639d7722e371b44860301e7d7
vvvyyynet <levyn.buerki@unibe.ch>
2026-03-23 12:57:17 +0100

    feat: minimal example

---
+++ src/routes/tei-test/1/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import CETEI from '$lib/CETEIcean/CETEI';
+	import { JSDOM } from 'jsdom';
+
+	const jdom = new JSDOM(`<TEI xmlns="http://www.tei-c.org/ns/1.0" />`, {
+		contentType: 'text/xml'
+	});
+	new CETEI({
+		documentObject: jdom.window.document
+	});
+</script>
---
+++ src/routes/tei-test/2/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import CETEI from '$lib/CETEIcean/CETEI';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		var CETEIcean = new CETEI();
+		CETEIcean.getHTML5('/test-tei/smallform_0231.xml', (data) => {
+			document.getElementById('TEI').appendChild(data);
+		});
+	});
+</script>
+
+<div id="TEI"></div>
+
+<style>
+	/* :global([type='place']) {
+		background-color: aqua;
+	} */
+</style>
