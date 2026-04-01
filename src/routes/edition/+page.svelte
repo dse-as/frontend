7a76868b73b0d6e4436016e912ca1680fb9810ad
vvvyyynet <levyn.buerki@unibe.ch>
2026-04-02 01:30:44 +0200

    feat: make edition overview as three scrollable columns

--- src/routes/edition/+page.svelte
+++ src/routes/edition/+page.svelte
@@ -4,25 +4,31 @@
 	let { data } = $props();
 </script>
 
-<div class="">
-	{#snippet list(meta)}
-		<ul class="my-4">
-			{#each Object.keys(meta) as key (key)}
-				<li>
+{#snippet list(meta)}
+	<ul class="my-4 h-full overflow-y-auto">
+		{#each Object.keys(meta) as key (key)}
+			<li>
 				<a href={resolve(`/edition/${key}`)}
 					>{meta[key].metadata.title_full} ({meta[key].metadata.year})</a
 				>
-				</li>
-			{/each}
-		</ul>
-	{/snippet}
+			</li>
+		{/each}
+	</ul>
+{/snippet}
 
-	<h1 class="mt-5 h1">Letters</h1>
-	{@render list(data.let_meta.letters_meta)}
+<div class="mb-20 flex h-[70vh] gap-25">
+	<div class="h-full w-100">
+		<h1 class="mt-5 h1">Letters</h1>
+		{@render list(data.let_meta.letters_meta)}
+	</div>
 
-	<h1 class="mt-5 h1">Longforms</h1>
-	{@render list(data.lgf_meta.longforms_meta)}
+	<div class="h-full w-100">
+		<h1 class="mt-5 h1">Longforms</h1>
+		{@render list(data.lgf_meta.longforms_meta)}
+	</div>
 
-	<h1 class="mt-5 h1">Smallforms</h1>
-	{@render list(data.smf_meta.smallforms_meta)}
+	<div class="h-full w-100">
+		<h1 class="mt-5 h1">Smallforms</h1>
+		{@render list(data.smf_meta.smallforms_meta)}
+	</div>
 </div>
