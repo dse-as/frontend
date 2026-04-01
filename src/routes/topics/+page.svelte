8fee216ee65155b981e17fb5c47fa4723d7cfbbc
vvvyyynet <levyn.buerki@unibe.ch>
2026-04-02 01:03:48 +0200

    feat: add topic pages

---
+++ src/routes/topics/+page.svelte
@@ -0,0 +1,34 @@
+<h1 class="h1">Sequenzen</h1>
+<p>
+	Vorgesehen ist hier eigentlich ein Reiter "Themen", aber für diesen Mockup sind hier mal alle
+	sequenzierbaren Listen zusammengestellt.
+</p>
+
+<h2 class="mt-10 h2">Publikationskontexte</h2>
+<nav class="mt-2">
+	<ul class="flex flex-wrap gap-2">
+		<li class="my-btn-round"><a href="/topics/textstufen">Textstufen</a></li>
+		<li class="my-btn-round"><a href="/topics/series">Serien</a></li>
+		<li class="my-btn-round"><a href="/topics/travels">Reisen</a></li>
+	</ul>
+</nav>
+<h2 class="mt-10 h2">Korrespondenzen</h2>
+<nav class="mt-2">
+	<ul class="flex flex-wrap gap-2">
+		<li class="my-btn-round">
+			<a href="/topics/correspondence">Korrespondenzen/Briefe</a>
+		</li>
+		<li class="my-btn-round"><a href="/topics/hotelbriefe">Hotelbriefe</a></li>
+	</ul>
+</nav>
+
+<h2 class="mt-10 h2">Thematisch</h2>
+<nav class="mt-2">
+	<ul class="flex flex-wrap gap-2">
+		<li class="my-btn-round"><a href="/topics/family">Familie</a></li>
+		<li class="my-btn-round"><a href="/topics/gender">Gender</a></li>
+		<li class="my-btn-round">
+			<a href="/topics/intermediality">Intermedialität</a>
+		</li>
+	</ul>
+</nav>
---
+++ src/routes/topics/correspondence/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	let { data } = $props();
+	let corrData = $derived(data.seqAll.correspondence);
+</script>
+
+<h1 class="h1">Briefwechsel</h1>
+
+<div class="mt-5 flex flex-wrap gap-2">
+	<button class="my-btn-round"
+		><a href={`/edition/${corrData['corr_spec_0001'].docs[0]}?seq=corr_spec_0001`}>Alle Briefe</a
+		></button
+	>
+	<button class="my-btn-round"
+		><a href={`/edition/${corrData['corr_spec_0002'].docs[0]}?seq=corr_spec_0002`}>Hotelbriefe</a
+		></button
+	>
+</div>
+
+<h2 class="mt-10 h2">Briefwechsel mit...</h2>
+<div class="mt-5 flex flex-wrap gap-2">
+	{#each Object.keys(corrData).filter((corrId) => {
+		return !['corr_spec_0001', 'corr_spec_0002'].includes(corrId);
+	}) as corrId}
+		{@const firstDocId = corrData[corrId].docs[0]}
+		<a
+			class={[
+				'my-btn-round',
+				!firstDocId && 'pointer-events-none border-surface-500 text-surface-500'
+			]}
+			href={`/edition/${firstDocId}?seq=${corrId}`}>{corrData[corrId].name}</a
+		>
+	{/each}
+</div>
