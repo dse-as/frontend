23b4c0d78a2f9f3ebbc1df066702801d5f8e8e81
vvvyyynet <levyn.buerki@unibe.ch>
2026-04-01 10:59:10 +0200

    fix: repair default size of thumbs

--- src/routes/edition/[docId=docId]/IIIF_Thumb.svelte
+++ src/routes/edition/[docId=docId]/IIIF_Thumb.svelte
@@ -5,12 +5,16 @@
 
 {#if url}
 	{#if isLoading}
-		<div class="flex h-full w-full items-center justify-center rounded-xl border">
+		<div
+			class="flex h-full w-full items-center justify-center rounded-xl border"
+			style={`min-width:${minSize}px; min-height:${minSize}px;`}
+		>
 			<i class="fa-solid fa-spinner fa-spin fa-2xl text-surface-500"></i>
 		</div>
 	{:else}
 		<img
-			class={`flex h-auto w-full items-center justify-center bg-surface-50 min-w-[${minSize}px] min-h-[${minSize}px] max-h-full max-w-full object-cover`}
+			class={`flex h-auto max-h-full w-full max-w-full items-center justify-center bg-surface-50 object-cover`}
+			style={`min-width:${minSize}px; min-height:${minSize}px;`}
 			src={`${url}/full/${minSize},/0/default.jpg`}
 			alt="iiif"
 		/>
