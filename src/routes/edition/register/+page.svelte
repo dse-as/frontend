7c9ce2b11a6f0ea63aac2cb7392b25f67615d000
vvvyyynet <levyn.buerki@unibe.ch>
2026-04-07 11:42:35 +0200

    doc: add documentation

--- src/routes/edition/register/+page.svelte
+++ src/routes/edition/register/+page.svelte
@@ -1,2 +1,9 @@
 <script lang="ts">
 </script>
+
+<!-- 
+Content is handled via +layout.svelte
+=====================================
+This is because the order of title and navigation flips 
+between +page and the subroute but there is only one {@render children()}
+-->
