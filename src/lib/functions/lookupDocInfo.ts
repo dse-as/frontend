d63e09be43644fd9295f19ce1701f7260afe2d69
vvvyyynet <levyn.buerki@unibe.ch>
2026-04-09 14:26:03 +0200

    fix: make lookupDocInfo more fail-proof with missing data

--- src/lib/functions/lookupDocInfo.ts
+++ src/lib/functions/lookupDocInfo.ts
@@ -1,11 +1,11 @@
 export function lookupDocInfo(docId, metadata): TItem {
-		return {
-			docId: docId,
-			fac: metadata[docId].manuscript.iiif_urls[0],
-			details: {
-				type: 'smallform', //! changethis
-				title: metadata[docId].metadata.title_full,
-				datestring: metadata[docId].metadata.pubDate
-			}
-		};
-	}
+	return {
+		docId: docId,
+		fac: metadata[docId]?.manuscript?.iiif_urls[0],
+		details: {
+			type: 'smallform', //! changethis
+			title: metadata[docId]?.metadata?.title_full,
+			datestring: metadata[docId]?.metadata?.pubDate
+		}
+	};
+}
