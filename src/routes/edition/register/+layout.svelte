e3e97882cb61870226cdded14dec5f05242e3c94
vvvyyynet <levyn.buerki@unibe.ch>
2026-04-10 17:37:18 +0200

    chore: reorder register layout

--- src/routes/edition/register/+layout.svelte
+++ src/routes/edition/register/+layout.svelte
@@ -2,7 +2,6 @@
 	import { dict_register as dictReg } from '$lib/dictionaries/dict_register.json';
 
 	let { data, children } = $props();
-	import { flip } from 'svelte/animate';
 
 	const regIdsForButtons = ['people', 'places', 'keywords', 'orgs', 'events', 'bibls'];
 
@@ -12,30 +11,7 @@
 	// - data.regType
 </script>
 
-<!-- Navigation -->
-{#if data.regSlug}
-	{@render nav()}
-{/if}
-
-<!-- Large Register-Menu  -->
-<!-- The animated transition uses the width of the element -->
-<h1
-	class={[
-		'absolute transition-all duration-400',
-		data.regView === 'regView1'
-			? 'top-45 left-0 w-full text-center h1'
-			: data.regView === 'regView2'
-				? 'top-45 left-0 w-1 pl-10  text-center h1 whitespace-nowrap'
-				: 'top-38 left-0 w-1 pl-10 text-center h4 whitespace-nowrap'
-	]}
->
-	{data.regView === 'regView1' ? 'Register' : dictReg[data.regType]?.register_name}
-</h1>
-<!-- Navigation for  -->
-{#if data.regView === 'regView1'}
-	{@render nav()}
-{/if}
-
+<!-- Snippet for Navigation -->
 {#snippet nav()}
 	<nav
 		class={[
@@ -53,12 +29,36 @@
 					data.regType === regId && 'my-btn-active'
 				]}
 				href={`/edition/register/${regId}`}
-				animate:flip={{ delay: 200 }}
 			>
 				{dictReg[regId]?.label_plural}
 			</a>
 		{/each}
 	</nav>
 {/snippet}
+
+<!-- Navigation -->
+{#if data.regView === 'regView2' || data.regView === 'regView3'}
+	{@render nav()}
+{/if}
+
+<!-- Large Register-Menu  -->
+<!-- The animated transition uses the width of the element -->
+<h1
+	class={[
+		'absolute transition-all duration-400',
+		data.regView === 'regView1'
+			? 'top-45 left-0 w-full text-center h1'
+			: data.regView === 'regView2'
+				? 'top-45 left-0 w-1 pl-10  text-center h1 whitespace-nowrap'
+				: 'top-38 left-0 w-1 pl-10 text-center h4 whitespace-nowrap'
+	]}
+>
+	{data.regView === 'regView1' ? 'Register' : dictReg[data.regType]?.register_name}
+</h1>
+
+<!-- Navigation -->
+{#if data.regView === 'regView1'}
+	{@render nav()}
+{/if}
 
 {@render children()}
