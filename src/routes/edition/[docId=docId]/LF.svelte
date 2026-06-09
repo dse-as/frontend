db3a87af69786473059df5bbb54fde806b4d3cc2
vvvyyynet <levyn.buerki@unibe.ch>
2026-06-09 14:01:58 +0200

    bits: convert tabs to bits-ui

--- src/routes/edition/[docId=docId]/LF.svelte
+++ src/routes/edition/[docId=docId]/LF.svelte
@@ -4,34 +4,37 @@
 	import Register from './Register.svelte';
 	import TextFluid from './TextFluid.svelte';
 
-	import { Tabs } from '@skeletonlabs/skeleton-svelte';
+	import { Tabs } from 'bits-ui';
 	type TActiveRegisterTab = 'notes' | 'register';
 
 	let { docId, docItem, ceteiData } = $props();
 </script>
 
-<div
-	data-fassung="LF"
-	class="grid h-full grid-cols-1 gap-25 overflow-hidden md:grid-cols-[auto_600px]"
->
+<div data-fassung="LF" class="grid grid-cols-1 gap-25 overflow-hidden md:grid-cols-[auto_600px]">
 	<TextFluid {ceteiData} />
 
-	<Tabs
-		class="mx-auto h-full w-full overflow-hidden pl-5 "
-		value={activeRegisterTab.value}
-		onValueChange={(details) => (activeRegisterTab.value = details.value as TActiveRegisterTab)}
-	>
-		<Tabs.List>
-			<Tabs.Trigger value="register" class="h5 hover:bg-transparent">Register</Tabs.Trigger>
-			<Tabs.Trigger value="notes" class="h5 hover:bg-transparent ">Stellenkommentare</Tabs.Trigger>
-			<Tabs.Indicator />
+	<Tabs.Root bind:value={activeRegisterTab.value}>
+		<Tabs.List
+			class="grid grid-cols-2 rounded-full border-[1.5px] border-surface-800-200 text-base leading-[0.01em] font-semibold"
+		>
+			<Tabs.Trigger
+				value="register"
+				class="h-9 rounded-l-full bg-transparent py-2 data-[state=active]:bg-tabs-active data-[state=active]:text-tabs-active-foreground"
+				>Register</Tabs.Trigger
+			>
+			<Tabs.Trigger
+				value="notes"
+				class="h-9 rounded-r-full bg-transparent py-2 data-[state=active]:bg-tabs-active data-[state=active]:text-tabs-active-foreground"
+				>Stellenkommentare</Tabs.Trigger
+			>
 		</Tabs.List>
+
 		<Tabs.Content value="register" class="h-full overflow-y-auto">
 			<Register {docId} {docItem} />
 		</Tabs.Content>
 
-		<Tabs.Content value="notes" class="h-full overflow-hidden">
+		<Tabs.Content value="notes" class="mt-5 h-full overflow-y-auto">
 			<Annotations {ceteiData} />
 		</Tabs.Content>
-	</Tabs>
+	</Tabs.Root>
 </div>
