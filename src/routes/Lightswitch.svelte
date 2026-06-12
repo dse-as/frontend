43399556f6705460c4b05c7802308db25ec783ca
vvvyyynet <levyn.buerki@unibe.ch>
2026-06-12 13:20:52 +0200

    fix: repair lightswitch appearance and add new dark/light colors

--- src/routes/Lightswitch.svelte
+++ src/routes/Lightswitch.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
-	import { toggleMode } from 'mode-watcher';
+	import { toggleMode, mode } from 'mode-watcher';
 	const iconClasses = 'h-[1.2rem] w-[1.2rem] rotate-0 !transition-all dark:-rotate-90';
 </script>
 
-<button onclick={toggleMode}>
-	<i class={['fa-solid fa-sun scale-100 dark:scale-0', iconClasses]}></i>
-	<i class={['fa-solid fa-moon scale-0 dark:scale-100', iconClasses]}></i>
+<button
+	onclick={toggleMode}
+	class="flex h-8 w-8 items-center justify-center rounded-full border-2 p-3 hover:border-light-40 hover:bg-light-10"
+>
+	{#if mode.current === 'dark'}
+		<i class={['fa-solid fa-sun text-xl', iconClasses]}></i>
+	{:else if mode.current === 'light'}
+		<i class={['fa-solid fa-moon text-xl', iconClasses]}></i>
+	{/if}
 	<span class="sr-only">Toggle theme</span>
 </button>
