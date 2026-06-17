d745b72fb30071759eabea979898fb6748aa56be
vvvyyynet <levyn.buerki@unibe.ch>
2026-06-17 16:24:44 +0200

    img: set up routes for images, [imgSeriesId] and [imgId]

--- src/routes/edition/images/+error.svelte
+++
@@ -1,7 +0,0 @@
-<!-- 
- 
-//! TODO
-    - if matcher returns `false`, redirect to /images with an alert box on top saying that the requested image/... does not exist.
-
--->
-<p>ERROR IN EDITION/IMAGES</p>
