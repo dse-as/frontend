bd725d54608fd80a1e337957bc8451e3e5c39210
vvvyyynet <levyn.buerki@unibe.ch>
2026-06-17 17:54:36 +0200

    fix: /photos

---
+++ src/routes/photos/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import DocumentsNav from '$lib/components/DocumentsNav.svelte';
+</script>
+
+<!-- Overview page with
+
+//! TODO
+ - links to image series
+ - form to enter image name (probably linked to search index)
+ 
+-->
+
+<!-- Navigation -->
+<DocumentsNav docType="photos" />
+
+<!-- Content -->
+<h1 class="h1">Fotografien</h1>
