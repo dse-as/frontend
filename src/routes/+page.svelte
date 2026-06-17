b63b1247166f0d3f35abcfcfe9e8e7ed68eb6303
vvvyyynet <levyn.buerki@unibe.ch>
2026-06-17 17:51:58 +0200

    feat: add shortcuts to landingpage as dropdown replacement

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { resolve } from '$app/paths';
+	import { dict_docs as dictDocs } from '$lib/dictionaries/dict_docs.json';
 </script>
 
 <div class="mx-auto mt-13 flex w-full max-w-300 flex-col items-center justify-center">
@@ -11,9 +12,27 @@
 	<p class="mt-5 -rotate-3 text-4xl font-bold text-red-500">Work in Progress!</p>
 </div>
 
-<div class="mt-20 text-center">
+<div class="mt-25 text-center">
 	<h2 class=" h2 mb-5">Shortcuts</h2>
-	<p class="text-xl">
-		&rarr; <a class="underline" href={resolve('/smallform_0231')}>Smallform 0231</a>
-	</p>
+
+	<h3 class="h3">Dropdown "Texte und Fotografien"</h3>
+	<ul class="mt-5 flex flex-col gap-2 text-2xl">
+		<li>&rarr; <a class="underline" href="/smallforms">{dictDocs.smallforms.label_plural}</a></li>
+		<li>&rarr; <a class="underline" href="/longforms">{dictDocs.longforms.label_plural}</a></li>
+		<li>&rarr; <a class="underline" href="/letters">{dictDocs.letters.label_plural}</a></li>
+		<li>&rarr; <a class="underline" href="/photos">{dictDocs.photos.label_plural}</a></li>
+	</ul>
+
+	<h3 class="h3 mt-15">Dropdown "Zugänge"</h3>
+	<ul class="mt-5 flex flex-col gap-2 text-2xl">
+		<li>&rarr; <a class="underline" href="/map">Karte</a></li>
+		<li>&rarr; <a class="underline" href="/timeline">Zeitstrahl</a></li>
+	</ul>
+
+	<h3 class="h3 mt-15">Dokumente</h3>
+	<ul class="mt-5 flex flex-col gap-2 text-2xl">
+		<li>
+			&rarr; <a class="underline" href={resolve('/smallform_0231')}>Smallform 0231</a>
+		</li>
+	</ul>
 </div>
