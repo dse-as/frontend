6898409aa3f0604f02b77141fddb202ebd75d1b3
vvvyyynet <levyn.buerki@unibe.ch>
2026-06-17 16:44:15 +0200

    fix: update paramMatcher for all dynamic routes

--- src/params/imgId.ts
+++ src/params/imgId.ts
@@ -1,13 +1,12 @@
 import type { ParamMatcher } from '@sveltejs/kit';
 import { documents as docs } from '$lib/data/documents.json';
 
-const keys_all = [
-	...Object.keys(docs.smallforms),
-	...Object.keys(docs.longforms),
-	...Object.keys(docs.letters)
-];
+// const keys_all = [
+//     ...Object.keys(docs.images)
+// ];
+const keys_all = [];
 
 export const match = ((param: string): param is (typeof keys_all)[number] => {
-	const result = keys_all.includes(param);
-	return result;
+    const result = keys_all.includes(param);
+    return result;
 }) satisfies ParamMatcher;
