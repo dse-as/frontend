d529a1e55fde9df0566a9789b6d0854a958c71da
vvvyyynet <levyn.buerki@unibe.ch>
2026-06-17 17:40:04 +0200

    fix: add missing resolve

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,13 @@
+<script lang="ts">
+	import { resolve } from '$app/paths';
+</script>
+
 <div class="mx-auto mt-13 flex w-full max-w-300 flex-col items-center justify-center">
 	<h1 class="h1">
 		Annemarie Schwarzenbach: Digitale Edition der Kleinen Formen und Briefe. Reisetexte,
 		Intermedialität, Netzwerke
 	</h1>
-	<h2 class="mt-35 -rotate-3 text-6xl font-bold text-red-500">Alpha-Version</h2>
+	<h2 class="mt-0 -rotate-3 text-6xl font-bold text-red-500">Alpha-Version</h2>
 	<p class="mt-5 -rotate-3 text-4xl font-bold text-red-500">Work in Progress!</p>
 </div>
 
