0de89ec371d68d836650f2881b90356d359e9b09
vvvyyynet <levyn.buerki@unibe.ch>
2026-06-25 12:08:40 +0200

    data: update dict and data

--- src/lib/types/TSequences.ts
+++ src/lib/types/TSequences.ts
@@ -32,6 +32,7 @@ export type TDictSeq = Record<
 	{
 		key_singular: string;
 		label_plural: string;
+		label_singular: string;
 		url_overview: string | null;
 		label_overview: string | null;
 		label_next: string;
