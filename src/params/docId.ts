f0f53d7b8fc72394b0c16ef2f7d109ccffcc9e59
vvvyyynet <levyn.buerki@unibe.ch>
2026-06-25 12:09:39 +0200

    fix: bring back letters to docId params

--- src/params/docId.ts
+++ src/params/docId.ts
@@ -1,8 +1,9 @@
 import type { ParamMatcher } from '@sveltejs/kit';
 import { smallforms_keys } from '$lib/data/smallforms_keys.json';
 import { longforms_keys } from '$lib/data/longforms_keys.json';
+import { letters_keys } from '$lib/data/letters_keys.json';
 
-const keys_all = [...smallforms_keys, ...longforms_keys];
+const keys_all = [...letters_keys, ...smallforms_keys, ...longforms_keys];
 
 export const match = ((param: string): param is (typeof keys_all)[number] => {
 	const result = keys_all.includes(param);
