8dcd23c8257fffb3f2b59b26329c3ffe73de02e3
vvvyyynet <levyn.buerki@unibe.ch>
2026-07-15 00:03:02 +0200

    feat: re-organise letters (part 1)

--- src/routes/letters/+page.svelte
+++ src/routes/letters/+page.svelte
@@ -1,44 +1,57 @@
 <script lang="ts">
-	import { resolve } from '$app/paths';
-	import DocumentsNav from '$lib/components/DocumentsNav.svelte';
 	import { doc_sequences as seqAll } from '$lib/data/doc_sequences.json';
 	import { type TSeqCorrespondenceKeys } from '$lib/types/TSequences';
+	import { resolve } from '$app/paths';
+	import { dict_docs as dictDoc } from '$lib/dictionaries/dict_docs.json';
+	import DocumentsNav from '$lib/components/DocumentsNav.svelte';
 
-	let corrData = $derived(seqAll.correspondence);
-	const specialCorrs = ['corr_spec_0001'];
+	let correspondendeData = $derived(seqAll.correspondence);
+	const specialCorrespondences = ['corr_spec_0001'];
+
+	let { data } = $props();
 </script>
 
-<!-- Navigation -->
-<DocumentsNav docType="letters" />
-
-<!-- Content -->
-<div class="mx-auto mt-13 flex w-full max-w-300 flex-col items-center justify-center">
-	<h1 class="h1">Korrespondenz</h1>
-	<div class="mt-10 flex flex-wrap justify-center gap-4">
-		<a
-			class="preset-btn-round --2xl"
-			href={resolve(`/${corrData['corr_spec_0001'].docs[0]}?seq=corr_spec_0001`)}>Alle Briefe</a
-		>
-	</div>
-
-	<h2 class="h2 mt-30">Spezifische Korrespondenzen</h2>
-	<div class="mt-10 flex flex-wrap justify-center gap-2">
-		{#each Object.keys(corrData).filter((corrId) => {
-			return !specialCorrs.includes(corrId);
-		}) as corrId (corrId)}
-			{@const firstDocId = corrData[corrId as TSeqCorrespondenceKeys].docs[0]}
-			{#if firstDocId}
-				<a class="preset-btn-round --lg" href={resolve(`/${firstDocId}?seq=${corrId}`)}
-					>{corrData[corrId as TSeqCorrespondenceKeys].name}</a
-				>
-			{:else}
-				<span class="preset-btn-round --lg --muted"
-					>{corrData[corrId as TSeqCorrespondenceKeys].name}</span
-				>
-			{/if}
+<!-- Series -->
+{#snippet keyList(keys: TSeqPhotoseriesKeys[])}
+	<div class={['preset-btn-list items-center justify-center', '--spacing-sm']}>
+		{#each keys as key (key)}
+			<a class="preset-btn-round --normal" href={resolve(`/${key}`)}
+				>{correspondendeData[key as TSeqCorrespondenceKeys].name}</a
+			>
 		{/each}
 	</div>
+{/snippet}
 
-	<h2 class="h2 mt-30">Korrespondenzen auf Reisen o.ä. ...</h2>
-	<div class="mt-10 flex flex-wrap justify-center gap-2"><span>TODO...</span></div>
+<div
+	data-dom="topStickyElement"
+	class={['z-100 flex flex-col bg-background px-10 ', 'mx-auto max-w-300 gap-10']}
+>
+	<!-- Navigation -->
+	<DocumentsNav docType="letters" />
+	<h1 class={['h1 text-center whitespace-nowrap transition-all duration-200']}>
+		{dictDoc['letters']?.label_plural}
+	</h1>
+	<div class={['flex flex-col gap-5']}>
+		<p>
+			Die Korrespondenz von Annemarie Schwarzenbach entstanden auf ihren Reisen durch Europa, den
+			Nahen Osten, Asien und die USA in den 1930er und 1940er Jahren. Erhalten sind einzig die
+			Briefe und Postkarten, welche Schwarzenbach gesendet hat.
+		</p>
+	</div>
+
+	<!-- Series Keys Large -->
+	<div class={['flex flex-col gap-25 py-5']}>
+		<div class="flex flex-col gap-7">
+			<h2 class="h2 text-center">Erhaltene Korrespondenzen</h2>
+			<div class="flex flex-wrap justify-center gap-2">
+				{@render keyList(Object.keys(correspondendeData) as TSeqCorrespondenceKeys[])}
+			</div>
+		</div>
+		<div class="flex flex-col gap-7 text-muted-foreground">
+			<h2 class="h2 text-center">Korrespondenzen auf Reisen o.ä. ...</h2>
+			<div class="flex flex-wrap justify-center gap-2">
+				<span>TODO...</span>
+			</div>
+		</div>
+	</div>
 </div>
