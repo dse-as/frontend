a33c6ad49fbd19b7ec3e9878c9c67a5b64d174d2
vvvyyynet <levyn.buerki@unibe.ch>
2026-07-24 14:58:30 +0200

    fix: show thumbnails
    remove lazy loading

--- src/lib/components/IIIF_Thumb.svelte
+++ src/lib/components/IIIF_Thumb.svelte
@@ -26,20 +26,23 @@
 
 {#if url}
 	{#if showSpinner}
-		<div class={['flex items-center justify-center', classes]}>
+		<div
+			class={['flex items-center justify-center', classes]}
+			data-url={adapt_iiif_url(url, iiif_imageAPI_width)}
+		>
 			<i class="fa-solid fa-spinner fa-spin fa-2xl text-dark-40"></i>
 		</div>
 	{/if}
 	{#if !isError}
 		<div class={['overflow-hidden', blur && 'blur-xs']}>
 			<img
-				loading="lazy"
 				class={[
 					blur && 'blur-lg',
 					showSpinner ? 'hidden' : 'flex',
 					'object-cover grayscale-60 hover:grayscale-0',
 					classes
 				]}
+				data-url={adapt_iiif_url(url, iiif_imageAPI_width)}
 				src={adapt_iiif_url(url, iiif_imageAPI_width)}
 				alt="iiif"
 				onload={() => {
